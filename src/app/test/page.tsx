dc7f66fb1bb2701300d5986ff738001f8eb7635c
Riya Vishwakarma <riya.vishwakarma@atomostech.com>
2024-10-28 17:24:54 +0530

    KDS-3 <Typography>

---
+++ src/app/test/page.tsx
@@ -0,0 +1,17 @@
+import Typography from "@/components/Typography";
+import React from "react";
+
+const Test = () => {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen gap-4">
+      <Typography variant="h1">H1 Headline</Typography>
+      <Typography variant="h2">H2 Headline</Typography>
+      <Typography variant="h3">H3 Headline</Typography>
+      <Typography variant="h4">H4 Headline</Typography>
+      <Typography variant="h5">H5 Headline</Typography>
+      <Typography variant="h6">H6 Headline</Typography>
+    </div>
+  );
+};
+
+export default Test;
--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { cn } from "@/utils/util";
+import { cva, VariantProps } from "class-variance-authority";
+import React, { ElementType, HTMLAttributes, ReactNode } from "react";
 
-const Typography = () => {
-  return <div>Typography</div>;
+interface TypographyProps
+  extends HTMLAttributes<HTMLHeadingElement>,
+    VariantProps<typeof typographyVariant> {
+  as?: ElementType;
+  children: ReactNode;
+}
+
+const typographyVariant = cva("text-dark font-bold", {
+  variants: {
+    variant: {
+      h1: "text-8xl",
+      h2: "text-7xl",
+      h3: "text-5xl",
+      h4: "text-[32px]",
+      h5: "text-2xl",
+      h6: "text-xl font-semibold",
+    },
+  },
+  defaultVariants: {
+    variant: "h1",
+  },
+});
+
+const Typography = ({
+  as,
+  variant = "h1",
+  children,
+  className,
+  ...props
+}: TypographyProps) => {
+  const Component = as || (variant as ElementType);
+  return (
+    <Component
+      className={cn(typographyVariant({ variant }), className)}
+      {...props}
+    >
+      {children}
+    </Component>
+  );
 };
 
 export default Typography;
