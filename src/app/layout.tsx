911209665b595864510ffc5750234e3803abe60e
Riya Vishwakarma <riya.vishwakarma@atomostech.com>
2024-10-28 10:14:22 +0530

    Font added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { cn } from "@/utils/util";
+import { Poppins } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Katalyst",
   description: "Website Design System",
 };
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  preload: true,
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("")}>{children}</body>
+      <body className={cn(poppins.className)}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <>Katalyst</>;
+  return <h1 className="text-xl font-bold">Katalyst</h1>;
 }
