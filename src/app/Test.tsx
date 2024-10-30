250f35a7cb8dcc283d159e0b1eedf30fd85767a9
Riya Vishwakarma <riya.vishwakarma@atomostech.com>
2024-10-30 14:27:56 +0530

    KDS-9 <Input Field>

--- src/app/Test.tsx
+++ src/app/Test.tsx
@@ -1,15 +1,17 @@
 "use client";
 import { Caption, Paragraph, Typography } from "@/components";
+import Input from "@/components/Input";
 import Label from "@/components/Label";
 import { useTheme } from "@/context/ThemeContext";
 import React from "react";
+import { RiAddCircleLine } from "react-icons/ri";
 
 const Test = () => {
   const { theme, switchDark, switchLight } = useTheme();
 
   return (
-    <div className="p-4 bg-light dark:bg-black min-h-screen">
-      <header className="p-4 transition-colors duration-300 flex justify-between items-center">
+    <div className="p-4 bg-light dark:bg-dark min-h-screen">
+      <header className="p-4 transition-colors duration-300 flex justify-between items-center sticky top-4 bg-light dark:bg-dark mx-4 rounded-full border">
         <Typography variant={"h6"}>Katalyst:</Typography>
         <Paragraph variant={"b2"}>Current theme: {theme}</Paragraph>
         <div className="space-x-2">
@@ -28,35 +30,81 @@ const Test = () => {
         </div>
       </header>
       <main className="space-y-5">
-        <Typography variant="h1">H1 Headline</Typography>
-        <Typography variant="h2">H2 Headline</Typography>
-        <Typography variant="h3">H3 Headline</Typography>
-        <Typography variant="h4">H4 Headline</Typography>
-        <Typography variant="h5">H5 Headline</Typography>
-        <Typography variant="h6">H6 Headline</Typography>
-        <Paragraph>The quick brown fox jumps over the lazy dog</Paragraph>
-        <Paragraph variant="b2">
-          The quick brown fox jumps over the lazy dog
-        </Paragraph>
-        <Paragraph variant="b3">
-          The quick brown fox jumps over the lazy dog
-        </Paragraph>
-        <Paragraph variant="b4">
-          The quick brown fox jumps over the lazy dog
-        </Paragraph>
-        <Caption variant="md">
-          The quick brown fox jumps over the lazy dog
-        </Caption>
-        <br />
-        <Caption>The quick brown fox jumps over the lazy dog</Caption>
-        <br />
-        <Label size={"sm"}>This is a Label</Label>
-        <br />
-        <Label size={"md"}>This is a Label</Label>
-        <br />
-        <Label size={"lg"} required>
-          This is a Label
-        </Label>
+        <section className="space-y-3">
+          <Typography variant="h6">Typography</Typography>
+          <Typography variant="h1">H1 Headline</Typography>
+          <Typography variant="h2">H2 Headline</Typography>
+          <Typography variant="h3">H3 Headline</Typography>
+          <Typography variant="h4">H4 Headline</Typography>
+          <Typography variant="h5">H5 Headline</Typography>
+          <Typography variant="h6">H6 Headline</Typography>
+        </section>
+        <section className="space-y-3">
+          <Typography variant="h6">Paragraph</Typography>
+          <Paragraph>The quick brown fox jumps over the lazy dog</Paragraph>
+          <Paragraph variant="b2">
+            The quick brown fox jumps over the lazy dog
+          </Paragraph>
+          <Paragraph variant="b3">
+            The quick brown fox jumps over the lazy dog
+          </Paragraph>
+          <Paragraph variant="b4">
+            The quick brown fox jumps over the lazy dog
+          </Paragraph>
+        </section>
+        <section className="space-y-3">
+          <Typography variant="h6">Caption</Typography>
+          <br />
+          <Caption variant="md">
+            The quick brown fox jumps over the lazy dog
+          </Caption>
+          <br />
+          <Caption>The quick brown fox jumps over the lazy dog</Caption>
+        </section>
+        <section className="space-y-3">
+          <Typography variant="h6">Label</Typography>
+          <Label size={"sm"}>This is a Label</Label>
+          <br />
+          <Label size={"md"}>This is a Label</Label>
+          <br />
+          <Label size={"lg"} required>
+            This is a Label
+          </Label>
+        </section>
+        <section className="space-y-5 min-h-[200px]">
+          <Typography variant="h6">Input</Typography>
+          <Input
+            type="text"
+            startIcon={<RiAddCircleLine />}
+            placeholder="Enter your text here"
+            endIcon={<RiAddCircleLine />}
+          />
+          <Input
+            type="text"
+            disabled
+            placeholder="Enter your text here"
+            startIcon={<RiAddCircleLine />}
+            endIcon={<RiAddCircleLine />}
+          />
+
+          <div className="space-y-5 min-h-[200px] p-10 div-glass">
+            <Input
+              type="text"
+              variant={"glass"}
+              startIcon={<RiAddCircleLine />}
+              placeholder="Enter your text here"
+              endIcon={<RiAddCircleLine />}
+            />
+            <Input
+              type="text"
+              variant={"glass"}
+              disabled
+              startIcon={<RiAddCircleLine />}
+              placeholder="Enter your text here"
+              endIcon={<RiAddCircleLine />}
+            />
+          </div>
+        </section>
       </main>
     </div>
   );
